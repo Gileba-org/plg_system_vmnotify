9621f26d489ac8547c52beb5a1a14ab684e9ab69
Gijs Lamon <info@gileba.be>
2023-11-03 00:09:39 +0100

    Basic files

---
+++ src/vmnotify.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author          Gileba <info@gileba.be>
+ * @link            https://gileba.be
+ * @copyright       Copyright © 2023 Gileba All Rights Reserved
+ * @license         GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
+ */
+
+defined("_JEXEC") or die("Restricted access");
+
+use Joomla\CMS\Plugin\CMSPlugin;
+
+class plgSystemGileba extends CMSPlugin
+{
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  3.1
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 *  Handling of protected extensions
+	 *  Throws a notice message if the Download Key is missing before downloading the package
+	 *
+	 *  @param   string  &$url      Update Site URL
+	 *  @param   array   &$headers
+	 */
+}
